04dce24b275be09e09706e6aa45bb71865a03b3b
timber3 <timber3@naver.com>
2022-06-24 22:37:48 +0900

    add HelloController, Dto, Test 2022.06.24 2nd push

---
+++ src/main/java/com/timber/admin/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.timber.admin.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
